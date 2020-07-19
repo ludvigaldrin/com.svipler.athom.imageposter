e67f046b06666057c537e6de7002ac03fc4e4b59
Ludvig Aldrin <ludvig.aldrin@gmail.com>
2020-07-19 17:26:18 +0200

    Updated

--- sendImage.php
+++ sendImage.php
@@ -3,8 +3,9 @@
 ini_set("log_errors", 1);
 ini_set("error_log", "sendImage-error.log");
 
+$id = uniqid();
 $file = 'sendImage-log.txt';
-file_put_contents($file, "\n" . date("h:i:sa") . "-> Start Receive", FILE_APPEND | LOCK_EX);
+file_put_contents($file, "\n" . date("h:i:sa") . " (" . $id . ") " . "-> Start Receive", FILE_APPEND | LOCK_EX);
 
 if (isset($_FILES['image'])) {
     $errors = array();
@@ -15,7 +16,7 @@ if (isset($_FILES['image'])) {
     $file_type = $_FILES['image']['type'];
     $file_ext = strtolower(end(explode('.', $_FILES['image']['name'])));
 
-    $file_name = time() . "." . $file_ext;
+    $file_name = time() . "-" . $id . "." . $file_ext;
 
     $extensions = array("jpeg", "jpg", "png");
 
@@ -29,15 +30,15 @@ $errors[] = 'File size must be excately 2 MB';
  **/
     if (empty($errors) == true) {
         move_uploaded_file($file_tmp, "images/" . $file_name);
-        file_put_contents($file, "\n" . date("h:i:sa") . "-> Success: File Uploaded: " . $file_name, FILE_APPEND | LOCK_EX);
+        file_put_contents($file, "\n" . date("h:i:sa") . " (" . $id . ") " . "-> Success: File Uploaded: " . $file_name, FILE_APPEND | LOCK_EX);
         echo "Success";
     } else {
-        file_put_contents($file, "\n" . date("h:i:sa") . "-> Error: " . $errors, FILE_APPEND | LOCK_EX);
+        file_put_contents($file, "\n" . date("h:i:sa") . " (" . $id . ") " . "-> Error: " . $errors, FILE_APPEND | LOCK_EX);
         echo "Error";
     }
 } else {
-    file_put_contents($file, "\n" . date("h:i:sa") . "-> Error: No Image attached", FILE_APPEND | LOCK_EX);
+    file_put_contents($file, "\n" . date("h:i:sa") . " (" . $id . ") " . "-> Error: No Image attached", FILE_APPEND | LOCK_EX);
     echo "Error";
 }
 
-file_put_contents($file, "\n" . date("h:i:sa") . "-> End Receive", FILE_APPEND | LOCK_EX);
+file_put_contents($file, "\n" . date("h:i:sa") . " (" . $id . ") " . "-> End Receive", FILE_APPEND | LOCK_EX);
